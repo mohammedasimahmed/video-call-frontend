6c09fd6f6ceb003849e6a9d94bbccfc9f3ae4e13
mohammedasimahmed <asim60999@gmail.com>
2024-03-16 16:07:44 +0530

    added .env file in client

--- utils/socketInitialize.js
+++ utils/socketInitialize.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
-export default function socketInitialize(){
-    const socket = io("http://localhost:5000")
-    return socket
+export default function socketInitialize() {
+  const socket = io(
+    process.env.BACKEND_URL ? process.env.BACKEND_URL : "http://localhost:5000"
+  );
+  return socket;
 }
